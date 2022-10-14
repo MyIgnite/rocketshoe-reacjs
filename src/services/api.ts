84bfaac136f6cf58e2c4accee93d6480935edd3f
Renan Moreira <renan.tsx@gmail.com>
2022-10-14 18:00:21 -0300

    edit baseURL api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/MyIgnite/rocketshoes-backend',
+  baseURL: 'https://my-json-server.typicode.com/MyIgnite/rocketshoes',
 });
